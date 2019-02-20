d2971289053d8b5c15d5a42749f2a5df96fbefa4
Dorisyluo <47812540+Dorisyluo@users.noreply.github.com>
2019-02-20 05:05:00 -0800

    .
    I kept getting wrong scene when I reopened it

---
+++ Assignment 5/Assets/menu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public GameObject restartmenu;
+
+    ParticleSystem system
+    {
+        get
+        {
+            if (_CachedSystem == null)
+                _CachedSystem = GetComponent<ParticleSystem>();
+            return _CachedSystem;
+        }
+    }
+    public ParticleSystem _CachedSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        restartmenu.SetActive(false);
+            }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (system.isPlaying)
+        {
+            display();
+        }
+
+    }
+    void display()
+    {
+        restartmenu.SetActive(true);
+    }
+    public void restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ Assignment 5/Assets/sticks.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class sticks: MonoBehaviour
+{
+    public static int s;
+
+    Text stick;
+
+    void Awake()
+    {
+        stick = GetComponent<Text>();
+        s = 0;
+    }
+
+
+    void Update()
+    {
+        stick.text = "You have " + s + " Sticks";
+    }
+}
